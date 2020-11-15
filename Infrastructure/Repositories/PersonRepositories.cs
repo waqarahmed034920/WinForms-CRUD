fb69ade54651a13053c9b88b10b2b76fed54d880
waqarahmed034920@gmail.com <waqarahmed034920@gmail.com>
2020-11-15 23:13:34 +0500

    complete person crud operation

---
+++ Infrastructure/Repositories/PersonRepositories.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using TaskManagementSystem.Model;
+
+namespace TaskManagementSystem.Infrastructure.Repositories
+{
+    public class PersonRepositories
+    {
+       
+
+        public bool insert(Person objPerson)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "insert into person(FirstName,lastName,Address,Phone,Email) values('" + objperson.FirstName + "','" + objperson.LastName + "','" + objperson.Address + "','" + objperson.Phone + "','" + objperson.Email + "')";
+
+            int noOfRowsAffected = command.ExecuteNonQuery();
+            connection.Close();
+            if (noOfRowsAffected >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool update(Person objPerson)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "update person set FirstName = '" + objupdateper.FirstName + "' ,lastName ='" + objupdateper.LastName + "',Address = '" + objupdateper.Address + "', phone= '" + objupdateper.Phone + "', Email='" + objupdateper.Email + "' where id = '" + objupdateper.Id + "'";
+
+            int noOfRowsAffected = command.ExecuteNonQuery();
+            connection.Close();
+            if (noOfRowsAffected >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "delete  from person where id = " + id.ToString();
+
+            int noOfRowsAffected = command.ExecuteNonQuery();
+            connection.Close();
+            if (noOfRowsAffected >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Person Getsingle(int id)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from person where id=" + id.ToString();
+            SqlDataReader myreader = command.ExecuteReader();
+            Person objPerson = null;
+            while (myreader.Read())
+            {
+                objPerson = new Person();
+                objPerson.Id = Convert.ToInt32(myreader["id"].ToString());
+                objPerson.FirstName = myreader["firstname"].ToString();
+                objPerson.LastName = myreader["lastname"].ToString();
+                objPerson.Address = myreader["address"].ToString();
+                objPerson.Phone = myreader["phone"].ToString();
+                objPerson.Email = myreader["email"].ToString();
+
+            }
+            myreader.Close();
+            connection.Close();
+            return objPerson;
+        }
+
+        public List<Person> GetAll()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from person";
+            SqlDataReader myreader = command.ExecuteReader();
+            List<Person> personlist = new List<Person>();
+            while (myreader.Read())
+            {
+                Person objperson = new Person();
+                objperson.Id = Convert.ToInt32(myreader["id"].ToString());
+                objperson.FirstName = myreader["firstname"].ToString();
+                objperson.LastName = myreader["lastname"].ToString();
+                objperson.Address = myreader["address"].ToString();
+                objperson.Phone = myreader["phone"].ToString();
+                objperson.Email = myreader["email"].ToString();
+                personlist.Add(objperson);
+            }
+            myreader.Close();
+            connection.Close();
+            return personlist;
+        }
+    }
+
+}
--- frmPersonAdd.cs
+++ frmPersonAdd.cs
@@ -8,18 +8,36 @@ using System.Text;
 using System.Windows.Forms;
 using TaskManagementSystem.Model;
 using static System.Windows.Forms.DataGridView;
-
+using TaskManagementSystem.Infrastructure.Repositories;
 namespace TaskManagementSystem
 {
     public partial class frmPersonAdd : Form
     {
         Person objPerson = null;
+        PersonRepositories repositories = new PersonRepositories();
         // Event driven programming.
         public frmPersonAdd()
         {
             InitializeComponent();
         }
+         void clearForm()
+        {
+            this.txtFirstName.Text = string.Empty;
+            this.txtLastName.Text = string.Empty;
+            this.txtAddress.Text = string.Empty;
+            this.txtPhone.Text = string.Empty;
+            this.txtEmail.Text = string.Empty;
+            txtFirstName.Focus();
+        }
+        private void frmperson_Load(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+        void RefreshGrid()
+        {
 
+            dgPerson.DataSource = this.repositories.GetAll();
+        }
         private void btnCancel_Click(object sender, EventArgs e)
         {
             DialogResult choice = MessageBox.Show("Are you show you want to leave the form?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
@@ -29,12 +47,13 @@ namespace TaskManagementSystem
             }
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
+        bool Validation()
         {
             if (txtFirstName.Text == string.Empty)
             {
                 MessageBox.Show("Please enter first name", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                 txtFirstName.Focus();
+                return false;
             }
             else
             {
@@ -42,6 +61,7 @@ namespace TaskManagementSystem
                 {
                     MessageBox.Show("Please enter lat name", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                     txtLastName.Focus();
+                    return false;
                 }
 
                 else
@@ -50,6 +70,7 @@ namespace TaskManagementSystem
                     {
                         MessageBox.Show("Please enter address", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                         txtAddress.Focus();
+                        return false;
                     }
                     else
                     {
@@ -57,6 +78,7 @@ namespace TaskManagementSystem
                         {
                             MessageBox.Show("Please enter phone", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                             txtPhone.Focus();
+                            return false;
                         }
                         else
                         {
@@ -64,159 +86,84 @@ namespace TaskManagementSystem
                             {
                                 MessageBox.Show("Please enter email.", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
                                 txtEmail.Focus();
+                                return false;
                             }
                             else
                             {
-                                bool output;
-                                string msg;
-                                if(objPerson == null)
-                                {
-                                    objPerson = new Person();
-                                    objPerson.FirstName = txtFirstName.Text;
-                                    objPerson.LastName = txtLastName.Text;
-                                    objPerson.Address = txtAddress.Text;
-                                    objPerson.Phone = txtPhone.Text;
-                                    objPerson.Email = txtEmail.Text;
-                                    output = savePerson(objPerson);
-                                    msg = "Record saved successfully.";
-                                }
-                                else 
-                                {
-                                    objPerson.FirstName = txtFirstName.Text;
-                                    objPerson.LastName = txtLastName.Text;
-                                    objPerson.Address = txtAddress.Text;
-                                    objPerson.Phone = txtPhone.Text;
-                                    objPerson.Email = txtEmail.Text;
-                                    output = updateperson(objPerson);
-                                    msg = "Record updated successfully.";
-                                }
-
-                                if (output == true)
-                                {
-                                    this.objPerson = null;
-                                    MessageBox.Show(msg, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                    clearForm();
-                                    RefreshGrid();
-                                }
-
-
+                                return true;
                             }
-
-
                         }
                     }
-
                 }
-
-
             }
-
         }
-
-        private void clearForm()
+        private void btnSave_Click(object sender, EventArgs e)
         {
-            this.txtFirstName.Text = string.Empty;
-            this.txtLastName.Text = string.Empty;
-            this.txtAddress.Text = string.Empty;
-            this.txtPhone.Text = string.Empty;
-            this.txtEmail.Text = string.Empty;
-        }
-
-        private bool savePerson(Person objperson)
-        {
-            try
+            if (this.Validation() == true)
             {
-                SqlConnection connection = new SqlConnection();
-                connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
-                connection.Open();
-
-                SqlCommand command = new SqlCommand();
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = "insert into person(FirstName,lastName,Address,Phone,Email) values('" + objperson.FirstName + "','" + objperson.LastName + "','" + objperson.Address + "','" + objperson.Phone + "','" + objperson.Email + "')";
-
-                int noOfRowsAffected = command.ExecuteNonQuery();
-                connection.Close();
-                if (noOfRowsAffected >= 1)
+                try
                 {
-                    return true;
+
+
+                      bool output;
+                      string msg;
+                      if (objPerson == null)
+                      {
+                          objPerson = new Person();
+                          objPerson.FirstName = txtFirstName.Text;
+                          objPerson.LastName = txtLastName.Text;
+                          objPerson.Address = txtAddress.Text;
+                          objPerson.Phone = txtPhone.Text;
+                          objPerson.Email = txtEmail.Text;
+                          output = this.repositories.insert(objPerson);
+                          msg = "Record saved successfully.";
+                      }
+                      else
+                      {
+                         objPerson.FirstName = txtFirstName.Text;
+                         objPerson.LastName = txtLastName.Text;
+                         objPerson.Address = txtAddress.Text;
+                         objPerson.Phone = txtPhone.Text;
+                         objPerson.Email = txtEmail.Text;
+                         output = this.repositories.update(objPerson);
+                         msg = "Record updated successfully.";
+                      }
+
+                      if (output == true)
+                      {
+                         this.objPerson = null;
+                         MessageBox.Show(msg, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                         clearForm();
+                         RefreshGrid();
+                      }
                 }
-                else
+                catch (Exception ex)
                 {
-                    return false;
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
+
             }
 
-        }
 
-        private bool updateperson(Person objupdateper)
-        {
 
-            try
-            {
-                SqlConnection connection = new SqlConnection();
-                connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
-                connection.Open();
+        } 
+                    
 
-                SqlCommand command = new SqlCommand();
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = "update person set FirstName = '" + objupdateper.FirstName + "' ,lastName ='" + objupdateper.LastName + "',Address = '" + objupdateper.Address + "', phone= '" + objupdateper.Phone + "', Email='" + objupdateper.Email + "' where id = '" + objupdateper.Id + "'";
+                
 
-                int noOfRowsAffected = command.ExecuteNonQuery();
-                connection.Close();
-                if (noOfRowsAffected >= 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-        }
 
-        private void frmperson_Load(object sender, EventArgs e)
-        {
-            RefreshGrid();
-        }
-        void RefreshGrid()
-        {
-            SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
-            connection.Open();
+            
 
-            SqlCommand command = new SqlCommand();
-            command.Connection = connection;
-            command.CommandType = CommandType.Text;
-            command.CommandText = "select * from person";
-            SqlDataReader myreader = command.ExecuteReader();
-            List<Person> personlist = new List<Person>();
-            while (myreader.Read())
-            {
-                Person objperson = new Person();
-                objperson.Id =  Convert.ToInt32(myreader["id"].ToString());
-                objperson.FirstName = myreader["firstname"].ToString();
-                objperson.LastName = myreader["lastname"].ToString();
-                objperson.Address = myreader["address"].ToString();
-                objperson.Phone = myreader["phone"].ToString();
-                objperson.Email = myreader["email"].ToString();
-                personlist.Add(objperson);
-            }
-            myreader.Close();
-            connection.Close();
-            dgPerson.DataSource = personlist;
-        }
+        
+
+       
+
+        
+
+       
+
+       
+       
 
         private void dgPerson_CellContextMenuStripNeeded(object sender, DataGridViewCellContextMenuStripNeededEventArgs e)
         {
@@ -231,29 +178,7 @@ namespace TaskManagementSystem
         {
             int id = Convert.ToInt32(dgPerson.CurrentRow.Cells[0].Value.ToString());
 
-            SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
-            connection.Open();
-
-            SqlCommand command = new SqlCommand();
-            command.Connection = connection;
-            command.CommandType = CommandType.Text;
-            command.CommandText = "select * from person where id="+id.ToString();
-            SqlDataReader myreader = command.ExecuteReader();
-
-            while (myreader.Read())
-            {
-                objPerson = new Person();
-                objPerson.Id = Convert.ToInt32(myreader["id"].ToString());
-                objPerson.FirstName = myreader["firstname"].ToString();
-                objPerson.LastName = myreader["lastname"].ToString();
-                objPerson.Address = myreader["address"].ToString();
-                objPerson.Phone = myreader["phone"].ToString();
-                objPerson.Email = myreader["email"].ToString();
-               
-            }
-            myreader.Close();
-            connection.Close();
+            this.objPerson = this.repositories.Getsingle(id);
             if (objPerson != null)
             {
                 this.txtFirstName.Text = objPerson.FirstName;
@@ -261,6 +186,10 @@ namespace TaskManagementSystem
                 this.txtAddress.Text = objPerson.Address;
                 this.txtPhone.Text = objPerson.Phone;
                 this.txtEmail.Text = objPerson.Email;
+            }
+            else
+            {
+                MessageBox.Show("Record not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -272,18 +201,9 @@ namespace TaskManagementSystem
             {
 
                 int id = Convert.ToInt32(dgPerson.CurrentRow.Cells[0].Value.ToString());
-                SqlConnection connection = new SqlConnection();
-                connection.ConnectionString = "server=waqar-pc\\sqlexpress; Database=aptech; trusted_connection=true;";
-                connection.Open();
-
-                SqlCommand command = new SqlCommand();
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = "delete  from person where id =" + id.ToString();
-
-                int noOfRowsAffected = command.ExecuteNonQuery();
-                connection.Close();
-                if (noOfRowsAffected >= 1)
+                bool ValueOutPut = this.repositories.Delete(id);
+                if(ValueOutPut ==true)
+               
                 {
                     MessageBox.Show("Record deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     RefreshGrid();
